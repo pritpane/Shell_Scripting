a9d2f270019e2bbe8e7fee92ac17165e989b431d
Pritam <pritpane@gmail.com>
2020-04-29 17:13:03 +0530

    Adding 1st Script

---
+++ basic-tasks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Author:Pritam Nipane
+#Date Created:07-April-2020
+#Description: This script will run linux basic task.
+#Date Modified: 07-April-2020 
+
+echo
+echo This script will run a few basic task
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+date
+echo
+cal 2020
+echo
+touch a b c
+echo End of scrift
+echo
