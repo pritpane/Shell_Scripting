f5c57c8ad5da1ca522712f563eb2797302fcda6b
Pritam <pritpane@gmail.com>
2020-04-29 17:18:46 +0530

    Adding 2nd Script

---
+++ basic-Admin-tasks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Author : Pritam Nipane
+#Created Date : 11-April-2020
+#Description: This script will run basic Administration commands.
+#Modified Date : 11-April-2020
+clear
+echo
+top | head -10
+echo
+df -h
+echo
+uptime
+echo
+free -m
+echo
+lsblk
+echo
+lscpu
+echo
+echo
+echo End of Script 
+echo
