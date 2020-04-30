a51dfc0165e29c2cf7be67c4da9da06f4b12fc28
Pritam <pritpane@gmail.com>
2020-04-30 20:20:09 +0530

    Adding 4th Script

---
+++ input-output.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Author:Pritam Nipane
+#Created date:11-April-2020
+#Description:This script will take input from user and print output.
+#Modified date:11-April-2020
+clear
+echo
+a=`hostname`
+echo Hello, My Server name is $a!
+echo
+echo What is your Name ?
+read b
+echo
+echo What is your Profession ?
+read c
+echo
+echo What is your Favrite show ?
+read d
+clear
+echo 
+echo Hello $b 
+echo
+echo Your Profession $c is awesome!
+echo
+echo $d is good show.
+echo
