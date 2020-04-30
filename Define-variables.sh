0962cafe9b60bd57ccd2338584cb240215f4c62f
Pritam <pritpane@gmail.com>
2020-04-30 20:17:10 +0530

    Adding 3rd Script

---
+++ Define-variables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Author:Pritam Nipane
+#Created date:11-April-2020
+#Description:This script will define variable.
+#Modified date:11-April-2020
+clear
+echo
+p=pwd
+l=ls
+wi=whoami
+d=date
+c='cal 2020'
+echo Run variable tasks
+echo
+$p
+echo
+$l
+echo
+$wi
+echo
+$d
+echo
+$c
+echo
+echo End of script 
