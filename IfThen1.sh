78d7a4225b0609d239cfb9c1735e8b7bed78974a
Pritam <pritpane@gmail.com>
2020-05-03 20:28:34 +0530

    Added IF Then 1 example Script

---
+++ IfThen1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Author:Pritam Nipane
+#Created date:03-May-2020
+#Description:This script will check the define variable.
+#Modified:
+echo
+count=100
+if [ $count -eq 100 ]
+then
+  echo Count is 100
+else
+  echo Count is not 100
+fi
+
